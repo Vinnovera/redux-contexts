bd0e769828269a84d2c9f53311e8e5185f2d8dea
jakobohman <jakob@vinnovera.se>
2017-10-12 15:13:40 +0200

    Missing exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 export contextConnect from './contextConnect';
 export contextFromState from './contextFromState';
 export ContextStoreProvider from './ContextStoreProvider';
+export {
+  createInjectStore,
+  injectReducer,
+  removeReducer
+} from './reduxInjector';
